f94775e3152f60f2ab091ee8675c5e764e2c7206
Nasly Katherine Escobar Ortiz <nkescobar@rapicredit.com>
2022-09-30 14:05:31 -0500

    implement web

--- infraestructure/entry-points/api-rest/src/main/java/com/makingapp/employee/api/EmployeeController.java
+++ infraestructure/entry-points/api-rest/src/main/java/com/makingapp/employee/api/EmployeeController.java
@@ -6,13 +6,13 @@ import com.makingapp.employee.service.EmployeeServiceImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/v1")
-@CrossOrigin
+@RequestMapping("/")
 public class EmployeeController {
     private static final Logger log = LoggerFactory.getLogger(EmployeeController.class);
 
@@ -30,6 +30,39 @@ public class EmployeeController {
     @PostMapping("/employee")
     public ResponseEntity<CustomResponse<Employee>> saveCategory(@RequestBody Employee request) {
         return employeeService.save(request);
+    }
+
+    @RequestMapping("/")
+    public String index(Model model) {
+        model.addAttribute("list", employeeService.find());
+        return "index";
+
+    }
+
+    @PostMapping("/guardar")
+    public String save(Employee empleado, Model model) {
+
+        employeeService.save(empleado);
+        return "redirect:/";
+    }
+
+    @GetMapping("/guardar/{id}")
+    public String showSave(@PathVariable("id") String id, Model model) {
+
+        if (id != null && !"0".equals(id)) {
+            model.addAttribute("empleado", employeeService.findById(id));
+        } else {
+            model.addAttribute("empleado", new Employee());
+        }
+
+        return "guardar";
+
+    }
+
+    @GetMapping("/eliminar/{id}")
+    public String delete(@PathVariable("id") String id, Model model) {
+        employeeService.delete(id);
+        return "redirect:/";
     }
 
 
